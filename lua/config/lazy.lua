df701a6e778821257d365bb014e497888fd9499c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-26 07:25:44 +0700

    Edit config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -44,6 +44,16 @@ require("lazy").setup({
             name = "catppuccin",
             priority = 1000,
         },
+        {
+            "amitds1997/remote-nvim.nvim",
+            version = "*", -- Pin to GitHub releases
+            dependencies = {
+                "nvim-lua/plenary.nvim", -- For standard functions
+                "MunifTanjim/nui.nvim", -- To build the plugin UI
+                "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+            },
+            config = true,
+        },
     },
     defaults = {
         -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
