d41e62a9479e4e1bc0f2319e0f58c5a5231f29f7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-09-09 09:42:31 +0700

    update

---
+++ lua/plugins/akinsho-bufferline.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "akinsho/bufferline.nvim",
+        keys = {
+            { "<leader>{", "<cmd>BufferLineMovePrev<CR>", desc = "Move buffer left" },
+            { "<leader>}", "<cmd>BufferLineMoveNext<CR>", desc = "Move buffer right" },
+        },
+    },
+}
